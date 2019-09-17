0f85461aefa63002002dcf35599e4dc34d7ab60b
Cesar Augusto Navarrete Pacheco <39611466+Rasner@users.noreply.github.com>
2019-09-17 15:50:14 -0500

    Initial commit

---
+++ Onigashima/Assets/Scripts/Main Menus/ButtonManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class ButtonManager : MonoBehaviour
+{
+    private void Start()
+    {
+
+    }
+    public void NewGame()
+    {
+        SceneManager.LoadScene("Tutorial");
+    }
+    public void Exit()
+    {
+        Application.Quit();
+    }
+    public void ReturMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+    public void Retry()
+    {
+
+    }
+    public void Credits()
+    {
+        SceneManager.LoadScene("Creditos");
+    }
+    public void Ajustes()
+    {
+        SceneManager.LoadScene("Ajustes");
+    }
+    public void Controles()
+    {
+        SceneManager.LoadScene("Controles");
+    }
+}
+
